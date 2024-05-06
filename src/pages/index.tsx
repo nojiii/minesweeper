886d30dbed8de3fe2905a0c12fea611a466dbec8
= <=>
2024-05-06 21:20:36 +0900

    マインスイーパー講習

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,63 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
+//useStateを減らす
+//機能を全部入れる(リプレイ不要)
+//初級～上級
+//カスタムサイズ
+//スマホ対応
+//要件通りに作る
+//計算値＝状態と計算値を加工して作る値
+//状態：時間で変化し保存される値=>board
+//状態はアプリを壊す
+//計算値：=>blackPoint boardから生成できる
 const Home = () => {
+  const [samplePos, setSamplePos] = useState(0);
+
+  const [bombMap, setBombMap] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const [userInputs, setUserInputs] = useState([]); //0~3 クリック 右クリック
+  //合成
+  const board: number[][] = [];
+  for (let y = 0; y < 9; y++) {
+    for (let x = 0; x < 9; x++) {
+      board.push(); // 0-8数字 9爆弾 10赤い爆弾 11
+    }
+  }
+  //tsx board.map
+  console.log(samplePos);
+
+  //   //再帰関数 0連鎖
+  //   const fn = (hikisuu) => {
+  //     if() {
+  // //0が続く限り開く
+  //     }else {
+  //       fn();
+  //     }
+  //     return;
+  //   };
+  //Reactの副作用
+  //useStateや計算値をもとに=>tsx->DOM(ブラウザ)
+  //UI = f(state)  Home(useState)
+
+  //useEffect 副作用を隔離する クリーンナップ 時計
+  //1s毎に再描画
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div
+        className={styles.sampleStyle}
+        style={{ backgroundPosition: `${samplePos * -30}px 0px` }} //cssスプライト
+      />
+      <button onClick={() => setSamplePos((p) => (p + 1) % 14)}>sample</button>
     </div>
   );
 };
